560761bb301a245aeb1e3947cbed4169dd5aaad4
denis <dineresc@gmail.com>
2020-06-27 17:45:11 -0300

    configurando h2 e dados de insert

---
+++ src/main/java/com/caminha/democacheable/DemoCacheableApplication.java
@@ -0,0 +1,13 @@
+package com.caminha.democacheable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCacheableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCacheableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caminha/democacheable/DemoCacheableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caminha.democacheable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCacheableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
